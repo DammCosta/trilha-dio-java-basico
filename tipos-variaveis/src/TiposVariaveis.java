4e54ae5437eacd5ba1e83ac1df80d53fb29dc317
Daniel Costa <90842689+DammCosta@users.noreply.github.com>
2023-02-20 20:17:58 -0300

    Fazendo testes

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // classe Operadores.java
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        String resultado = (a==b) ? "verdadeiro" : "false";
+        
+        System.out.println(resultado);
     }
 }
